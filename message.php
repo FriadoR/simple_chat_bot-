a6cedeb84e5e25f084bc5edb61a4db59a3031c52
Sachenko Sergey <braun.krieg@googlemail.com>
2025-03-02 00:22:17 +0300

    final commit, all works fine

--- message.php
+++ message.php
@@ -17,11 +17,19 @@ $conn = mysqli_connect($servername, $username, $password, $dbname);
 $getMesg = mysqli_real_escape_string($conn, $_POST['text']);
 
 // cheking user query to database query
-$check_data = "SELECT replies FROM chatBot WHERE queries LIKE '%$getMesg%'";
+$check_data = "SELECT replies FROM chat WHERE queries LIKE '%$getMesg%'";
 $run_query = mysqli_query($conn, $check_data) or die("Error run query");
 
-
-
+// if user query matched to database query we'll show the reply otherwise it go to else statement
+if(mysqli_num_rows($run_query) > 0) {
+// fetching replay from the database according to the user query
+    $fetch_data = mysqli_fetch_assoc($run_query);
+    // storing replay to a variable which we'll send to ajax
+    $replay = $fetch_data['replies'];
+    echo $replay;
+} else {
+    echo "Sorry, can't be able to undestand you!";
+}
 
 
 ?>
